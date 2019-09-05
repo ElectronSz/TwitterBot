1115053b4a04c25a0190b12926ef4efd22c34bb6
EletronSz <janetuker2@gmail.com>
2019-09-05 08:35:43 +0200

    stageed

--- app.py
+++ app.py
@@ -6,30 +6,35 @@ class TBot:
     def __init__(self,username,password):
         self.username = username
         self.password = password
-        self.bot = webdriver.Chrome()
+        self.bot = webdriver.Firefox(executable_path="C:\Program Files (x86)\Python36-32\geckodriver.exe")
 
     def login(self):
         bot = self.bot
         bot.get("https://twitter.com/login")
+        time.sleep(3)
         email = bot.find_element_by_class_name("js-username-field")
         password = bot.find_element_by_class_name("js-password-field")
       
         email.send_keys(self.username)
         password.send_keys(self.password)
-
         password.send_keys(Keys.RETURN)
+        time.sleep(3)
 
     def like_tweet(self, hashtag):
         bot = self.bot
         bot.get('https://twitter.com/search?q='+hashtag+'&src=typd')
-        time.sleep(5)
+        time.sleep(3)
 
-        for i in range(1,3):
+        for i in range(1,10):
             bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-            time.sleep(5)
-            tweets = bot.find_elements_by_class_name('r-1adg3ll')
+            time.sleep(3)
+            tweets = bot.find_elements_by_class_name('tweet')
             links = [elem.get_attribute('data-permalink-path') for elem in tweets]
-            print(links)
+            
+            for link in links:
+                bot.get('https://twitter.com'+ link)
+                bot.find_element_by_class_name('HeartAnimation').click()
+                time.sleep(10)
         
 
 
